2b7edaeb2acbd3612cb9ffd806baf7abd6dd847e
cvarshini22 <varshaappaji1999@gmail.com>
2019-05-11 11:15:27 +0530

    java

--- js/script.js
+++ js/script.js
@@ -13,5 +13,32 @@ function marks(num) {
     }
 
 }
+
+function c() {
+    console.log("called c function");
+}
+function b() {
+    c();
+    console.log("called b function");
+}
+function a() {
+    b();
+    console.log("called a function");
+}
+a();
+
+function integration() {
+    console.log("integration");
+}
+function diff() {
+    console.log("diff");
+}
+function greetUser(firstName, lastName) {
+    console.log("Hello" + firstName + " " + lastName);
+}
+integration();
+diff();
+greetUser(" varshini C appaji ");
 var num = prompt("enter the number");
 marks(num);
+
